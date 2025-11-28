d365afc86c2cb6aef58076cafb419064de0b69b0
yaroslavn <yaroslav.n@open-tag.com>
2025-11-28 22:31:29 +0200

    Slider: created Events for prev, next and setIntarval.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,53 @@
 const slider = (function (){
   let curentIndex = 0;
  
+  //Events
+  const events = (function(){
+    let prev = null;
+    let next = null;
+    let timer = null;
 
-  const prev = document.getElementById('slider-prev');
-  const next = document.getElementById('slider-next');
+    let prevFn = null;
+    let nextFn = null;
+
+    const clickEvents = {
+      onPrev(handelEvent) {
+        prevFn = handelEvent;
+        prev.addEventListener('click', prevFn);
+      },
+
+      onNext(handelEvent) {
+        nextFn = handelEvent;
+        next.addEventListener('click', nextFn);
+      }
+    }
+
+    const intervalEvents = {
+      start(handelInterval, duration = 1000){
+        timer = setInterval(handelInterval, duration);
+      },
+
+      stop(){
+        clearInterval(timer);
+      }
+    }
+
+    const init = () => {
+      prev = document.getElementById('slider-prev');
+      next = document.getElementById('slider-next');
+
+      return {
+        click: clickEvents,
+        interval: intervalEvents,
+      }
+    }
+
+    return {
+      init,
+    }
+
+  }());
+
   const items = document.querySelectorAll('.slider__item');
 
   const getIndex = (curentIndex) => curentIndex - 5 * (Math.floor(curentIndex / 5));
@@ -11,13 +55,21 @@ const slider = (function (){
   const nextIndex = () => getIndex(++curentIndex);
   const curentPosition = () => getIndex(curentIndex);
 
-  prev.addEventListener('click', () => {
+  const eventsInstance = events.init();
+
+  eventsInstance.interval.start(() => {
+    renderSlide(nextIndex);
+  })
+
+  eventsInstance.click.onPrev(() => {
     renderSlide(prevIndex);
   });
 
-  next.addEventListener('click', () => {
+  eventsInstance.click.onNext(() => {
     renderSlide(nextIndex);
-  });
+  })
+
+
 
   const renderSlide = (action) => {
     const prev = curentPosition();
@@ -28,10 +80,9 @@ const slider = (function (){
     items[curent].style.opacity = 1;
   }
 
-  setInterval(() => {
+/*   setInterval(() => {
     renderSlide(nextIndex)
-  }, 1000);
-
+  }, 1000); */
 
   const init = () =>{
 
